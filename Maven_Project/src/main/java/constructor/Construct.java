83c0994710f46907013fd1cc70a119b6e3b7e67b
Sony11M <sonymadathil11@gmail.com>
2023-07-06 11:01:47 +0530

    commit and push

---
+++ Maven_Project/src/main/java/constructor/Construct.java
@@ -0,0 +1,23 @@
+package constructor;
+
+public class Construct 
+{
+
+	
+		public Construct()     //Default constructor creation
+		{
+			System.out.println();
+			int age= 10;
+		    String name= "Sony";
+		    System.out.println(age);
+		    System.out.println(name);
+		}
+		
+
+	public static void main(String[] args)
+	{
+		Construct con= new Construct();    
+
+	}
+
+}
